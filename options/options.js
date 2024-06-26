5942a13fa99548c98a93b7bff71b807ef404e963
Crowdbotics <team@crowdbotics.com>
2024-06-26 00:04:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-rain-48551",
+  url: "https://aged-rain-48551.botics.co",
+  api: "https://aged-rain-48551.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
